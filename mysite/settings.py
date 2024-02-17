0f77b10d77e491d011e2a28ee8016c9d48668f56
Oybekt <tursunboyevjahongir187@gmail.com>
2024-02-17 15:00:39 +0500

    static fayllari o'zgartirildi

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,7 +112,7 @@ STATIC_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]
 
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
