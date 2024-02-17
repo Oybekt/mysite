114045f2a65b772551373e1b20b72b2b06c3428a
Oybekt <tursunboyevjahongir187@gmail.com>
2024-02-17 15:43:54 +0500

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import logging
 import dj_database_url
+import sys
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,9 +81,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    "defult": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-}
+if DEBUG is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "defult": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
